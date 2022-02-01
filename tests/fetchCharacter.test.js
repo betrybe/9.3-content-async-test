3ca272372c955cf0de5d0f82127af7e95b9b39d6
vinidipaula <vinidipaula92@gmail.com>
2022-02-01 18:20:30 -0300

    Exercicio fetch realizado

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -5,12 +5,24 @@ describe('descrição geral', () => {
 /*   it('descrição', async () => {
     await requisicao()
   }); */
-/*   it('Verifica se o nome da personagem é Wonder Woman', async () => {
+  it('Verifica se o nome da personagem é Wonder Woman', async () => {
     const character = await fetchCharacter('720');
     expect(character.name).toBe('Wonder Woman');
-  }); */
+  });
   it('Verifica se retorna erro ao executar a função sem parâmetro', async () => {
     const failRequest = await fetchCharacter();
     expect(failRequest).toEqual(new Error('You must provide an url'));
-  })
+  });
+
+  it('Verifica se retorna \'Invalid id\' ao executar a função com parâmetro que não existe', async () => {
+    const response = await fetchCharacter('parametro qualquer');
+    expect(response).toBe('Invalid id'); // falso positivo 
+  });
+
+  it('Verifica se fetch é chamada com o endpoint correto', async () => {
+    const url = 'https://www.superheroapi.com/api.php/4192484924171229/720';
+    await fetchCharacter('720');
+    expect(fetch).toHaveBeenCalledTimes(4); // Verificando se a função fetch foi chamada 4 vezes
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
 });
