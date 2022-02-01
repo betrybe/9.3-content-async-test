3974e451db1666d4450b08d23d7cd7c3921a864f
Thiago <thiagoedusan5.11@outlook.com>
2022-02-01 19:07:30 -0300

    Exercicio guiado

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -2,8 +2,10 @@ require('../mocks/fetchSimulator');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
 describe('Teste a função fetchCharacter', () => {
-  it('Descrição', async () => {
-    const result = await fetchCharacter();
-    expect(result).toEqual(new Error('You must provide an url'));
+  it('Verifica se fetch é chamada com o endpoint correto', async () => {
+    const url = 'https://www.superheroapi.com/api.php/4192484924171229/720';
+    await fetchCharacter('720');
+    expect(fetch).toHaveBeenCalledTimes(4);
+    expect(fetch).toHaveBeenCalledWith(url);
   });
 });
