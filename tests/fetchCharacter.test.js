6f02407aeee6296ebe0af6aa975cc01582166d9c
Geovani Moura <94002157+GeovaniMoura@users.noreply.github.com>
2022-02-01 11:50:33 -0300

    done

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -3,6 +3,20 @@ const { fetchCharacter } = require('../src/fetchCharacter');
 
 describe('Teste a função fetchCharacter', () => {
   it('', async () => {
-    await requisicao();
+    const character = await fetchCharacter('720');
+    expect(character.name).toEqual('Wonder Woman');
+  });
+
+  it('Verifica se retorna erro ao executar a função sem parâmetro', async () => {
+    const failRequest = await fetchCharacter();
+    console.log(failRequest);
+    expect(failRequest).toEqual(new Error('You must provide an url'));
+  });
+
+  it('Verifica se fetch é chamada com o endpoint correto', async () => {
+    const url = 'https://www.superheroapi.com/api.php/4192484924171229/720';
+    await fetchCharacter('720');
+    expect(fetch).toHaveBeenCalledTimes(4);
+    expect(fetch).toHaveBeenCalledWith(url);
   });
 });
