49c2aaa5de93029d8de09eac54d93ae7d8aa8ed5
Thiago <thiagoedusan5.11@outlook.com>
2022-02-01 19:02:05 -0300

    Exercicio guiado

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -2,5 +2,8 @@ require('../mocks/fetchSimulator');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
 describe('Teste a função fetchCharacter', () => {
-  it('', () => {});
+  it('Descrição', async () => {
+    const result = await fetchCharacter();
+    expect(result).toEqual(new Error('You must provide an url'));
+  });
 });
