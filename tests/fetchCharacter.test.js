71f20658895909f7ae2216d5a0085b60c9d69613
Tiemi Faustino <hayashipharma@gmail.com>
2022-02-01 14:34:15 -0300

    acompanhamento bloco 9.3

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -2,5 +2,12 @@ require('../mocks/fetchSimulator');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
 describe('Teste a função fetchCharacter', () => {
-  it('', () => {});
+  // it('descrição do que está sendo testado', async () => {
+  //   await requisicao();
+  // });
+
+  it('Verifica se o nome da personagem é Wonder Woman', async () => {
+    const character = await fetchCharacter('720');
+    console.log(character);
+  });
 });
