b67f4339c13f3208cf8d53347ed4d19ee05d1c4d
FrancianeMenestrina <francianemenestrina@gmail.com>
2022-03-23 19:38:47 -0300

    Ajustes

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -1,6 +1,10 @@
 require('../mocks/fetchSimulator');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
-describe('Teste a função fetchCharacter', () => {
-  it('', () => {});
+describe('descrição geral', () => {
+  it('Verifica se o nome da personagem é Wonder Woman', async () => {
+    const character = await fetchCharacter('720');
+    expect(character.name).toBe('Wonder Woman');
+  });
 });
+
