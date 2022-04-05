3678b749fd371cb9a9bfac71e49f3dbfb695b8ba
Gabriel Franca <gabrielcfranca@hotmail.com>
2022-04-04 21:51:21 -0300

    conteudo do dia

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -2,5 +2,9 @@ require('../mocks/fetchSimulator');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
 describe('Teste a função fetchCharacter', () => {
-  it('agora sim!', () => {});
+  it('agora sim!', async () => {
+    const character = await fetchCharacter('720');
+    console.log(character.name);
+    expect(character.name).toBe('spider Man');
+  });
 });
